cd9fafb94c061da517a85b969aaea78bb213a47a
FazlayRafi <rafi.rainbowit01@gmail.com>
2023-12-25 16:15:57 +0600

    Complete kindergarten

--- components/Header/Headers/Header-Six.js
+++ components/Header/Headers/Header-Six.js
@@ -3,11 +3,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useAppContext } from "@context/Context";
 
 import logo from "../../../public/images/logo/logo.png";
 
 import Nav from "../Nav";
+import { useAppContext } from "@/context/Context";
 
 const HeaderSix = ({
   gapSpaceBetween,
--- pages/04-kindergarten/index.js
+++ pages/04-kindergarten/index.js
@@ -1,12 +1,14 @@
-import MobileMenu from "@/components/Header/MobileMenu";
-import PageHead from "../Head";
-// import HeaderStyleEight from "@/components/Header/HeaderStyle-Eight";
-import Kindergarten from "@/components/04-kindergarten/04-kindergarten";
 import { Provider } from "react-redux";
 import Store from "@/redux/store";
 import Context from "@/context/Context";
+import PageHead from "../Head";
+
+import MobileMenu from "@/components/Header/MobileMenu";
+import Kindergarten from "@/components/04-kindergarten/04-kindergarten";
 import Cart from "@/components/Header/Offcanvas/Cart";
 import FooterOne from "@/components/Footer/Footer-One";
+import BackToTop from "../backToTop";
+import HeaderStyleEight from "@/components/Header/HeaderStyle-Eight";
 
 const Home = () => {
   return (
@@ -15,11 +17,11 @@ const Home = () => {
       <Provider store={Store}>
         <Context>
           <MobileMenu />
-          {/* <HeaderStyleEight headerSticky="rbt-sticky" headerType="" /> */}
+          <HeaderStyleEight headerSticky="rbt-sticky" headerType="" />
           <Kindergarten />
           <Cart />
 
-          {/* <Separator /> */}
+          <BackToTop />
           <FooterOne />
         </Context>
       </Provider>
