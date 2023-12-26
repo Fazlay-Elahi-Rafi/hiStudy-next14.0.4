c0fff689f6946b4f6c18f4648e3ba1bb790dfd25
FazlayRafi <rafi.rainbowit01@gmail.com>
2023-12-26 17:40:35 +0600

    add Brand

---
+++ pages/elements/brand/index.js
@@ -0,0 +1,50 @@
+import Context from "@/context/Context";
+import PageHead from "@/pages/Head";
+import Store from "@/redux/store";
+import { Provider } from "react-redux";
+
+import BrandOne from "@/components/Brand/Brand-One";
+import BrandThree from "@/components/Brand/Brand-Three";
+import BrandTwo from "@/components/Brand/Brand-Two";
+import BreadCrumb from "@/components/Common/BreadCrumb";
+import Separator from "@/components/Common/Separator";
+import FooterOne from "@/components/Footer/Footer-One";
+import HeaderStyleTen from "@/components/Header/HeaderStyle-Ten";
+import MobileMenu from "@/components/Header/MobileMenu";
+import Cart from "@/components/Header/Offcanvas/Cart";
+
+const BrandPage = () => {
+  return (
+    <>
+      <PageHead title="Brand - Online Courses & Education Bootstrap5 Template" />
+
+      <Provider store={Store}>
+        <Context>
+          <HeaderStyleTen headerSticky="rbt-sticky" headerType="" />
+          <MobileMenu />
+          <Cart />
+          <BreadCrumb title="Brand" text="Brand" />
+
+          <div className="rbt-brand-area bg-color-white rbt-section-gap">
+            <div className="container">
+              <BrandOne />
+            </div>
+          </div>
+
+          <div className="rbt-brand-area bg-color-extra2 rbt-section-gap">
+            <BrandTwo />
+          </div>
+
+          <div className="rbt-brand-area bg-color-white rbt-section-gap">
+            <BrandThree />
+          </div>
+
+          <Separator />
+          <FooterOne />
+        </Context>
+      </Provider>
+    </>
+  );
+};
+
+export default BrandPage;
