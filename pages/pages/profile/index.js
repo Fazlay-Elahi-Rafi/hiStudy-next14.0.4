91d16fd47aab1f1ef7acf14001c8c4b1fe87ff70
FazlayRafi <rafi.rainbowit01@gmail.com>
2023-12-27 09:45:56 +0600

    add Profile

---
+++ pages/pages/profile/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Context from "@/context/Context";
+import { Provider } from "react-redux";
+import Store from "@/redux/store";
+import { useRouter } from "next/router";
+
+import Separator from "@/components/Common/Separator";
+import FooterOne from "@/components/Footer/Footer-One";
+import HeaderStyleTen from "@/components/Header/HeaderStyle-Ten";
+import MobileMenu from "@/components/Header/MobileMenu";
+import Cart from "@/components/Header/Offcanvas/Cart";
+import PageHead from "@/pages/Head";
+import BackToTop from "@/pages/backToTop";
+
+const ProfileLayout = () => {
+  const router = useRouter();
+  const postId = router.query.profileId;
+
+  useEffect(() => {
+    if (postId === undefined) {
+      router.push("/pages/profile/1");
+    }
+  }, []);
+
+  console.log(postId);
+  return (
+    <>
+      <PageHead title="Profile - Online Courses & Education Bootstrap5 Template" />
+      <Provider store={Store}>
+        <Context>
+          <MobileMenu />
+          <HeaderStyleTen headerSticky="rbt-sticky" headerType="" />
+          <Cart />
+
+          <BackToTop />
+          <Separator />
+          <FooterOne />
+        </Context>
+      </Provider>
+    </>
+  );
+};
+
+export default ProfileLayout;
